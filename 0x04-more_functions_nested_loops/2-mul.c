81931f1541bc390bb97ade73321eb4bd3802850a
Sungbawiera <azizcr7sungbawiera700@gmail.com>
2023-03-24 05:27:38 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - The function called by main
+ *
+ * @a: The first parameter of the function
+ * @b: The second parameter of the function
+ *
+ * Description: Write a function that multiplies two integers.
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
