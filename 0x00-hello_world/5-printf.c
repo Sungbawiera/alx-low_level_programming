7023bc102b67c07fefd3e963c4ae7da8a93fbd4c
Sungbawiera <azizcr7sungbawiera700@gmail.com>
2023-04-02 06:02:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
- * Return: 0
- */
+*main - Entry Point
+*Return: 0
+*/
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);	
+	return (0);
 }	
